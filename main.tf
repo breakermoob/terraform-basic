36c37c1f7bb50379c72ebda1dc8ec575c23d0440
Breakermoob <breakermoob@hotmail.com>
2020-04-04 12:04:53 -0500

    Terraform - AWS Cluster with a Load Balancer

--- main.tf
+++ main.tf
@@ -7,6 +7,10 @@ variable "server_port" {
   description = "this is the port of the server, we will use for the HTTP request"
   default     = 8080
 }
+variable "lb_port" {
+  description = "this is the port of the server, we will use for the HTTP request"
+  default     = 80
+}
 
 //Return a Json with the availability zones
 data "aws_availability_zones" "all" {}
@@ -36,6 +40,10 @@ resource "aws_autoscaling_group" "example" {
 
   //indicate the name of zones
   availability_zones = "${data.aws_availability_zones.all.names}"
+
+  //Load Balancer and Health check
+  load_balancers    = ["${aws_elb.example.name}"]
+  health_check_type = "ELB"
 
   //min and max number of machines
   min_size = 2
@@ -70,6 +78,60 @@ resource "aws_security_group" "instance" {
     create_before_destroy = true
   }
 }
+
+//elastic load balancer
+resource "aws_elb" "example" {
+
+  name = "terraform-leon-cluster"
+
+  availability_zones = "${data.aws_availability_zones.all.names}"
+  security_groups    = ["${aws_security_group.elb.id}"]
+
+  listener {
+
+    lb_port     = "${var.lb_port}"
+    lb_protocol = "http"
+
+    instance_port     = "${var.server_port}"
+    instance_protocol = "http"
+  }
+
+  //This send an internal request each 30 seconds
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    interval            = 30
+    target              = "HTTP:${var.server_port}/"
+  }
+
+}
+
+resource "aws_security_group" "elb" {
+  name = "terraform-leon-elb"
+
+  ingress {
+    from_port   = "${var.lb_port}"
+    to_port     = "${var.lb_port}"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  //Open all ports
+  egress {
+    from_port = 0
+    to_port   = 0
+    //all protocols
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "elb_dns_name" {
+  value = "${aws_elb.example.dns_name}"
+}
+
+
 
 
 #Terraform init for create the proyect structure
